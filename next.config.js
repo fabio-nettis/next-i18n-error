fe1d035be33ee42b1a59dce0fb17cf7fa9270a32
Fabio Nettis <nettisfabio@gmail.com>
2023-08-08 12:36:49 +0200

    feat: break app with i18n configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ["default", "de", "fr", "it"],
+    defaultLocale: "default",
+  },
+};
 
 module.exports = nextConfig;
